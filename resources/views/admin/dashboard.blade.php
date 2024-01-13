8d2584a402b748f5f71c4e412244ab422e32aae9
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-01-13 09:35:45 +0200

    dashboard serverside on thirdside again

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -160,7 +160,7 @@
             <div class="table-data">
                 <div class="order">
                     <div class="head">
-                        <h3>Recent Orders</h3>
+                        <h3>Recenty Posted Products</h3>
                         <i class='bx bx-search'></i>
                         <i class='bx bx-filter'></i>
                     </div>
@@ -168,18 +168,18 @@
                         <thead>
                             <tr>
                                 <th>User</th>
-                                <th>Date Order</th>
-                                <th>Status</th>
+                                <th>Date Posted</th>
+                               
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>
                                     <img src="{{ asset('images/modern.jpg') }}" />
-                                    <p>Alain Nig</p>
+                                    <p></p>
                                 </td>
                                 <td>01-10-2021</td>
-                                <td><span class="status completed">Completed</span></td>
+                                
                             </tr>
                             <tr>
                                 <td>
@@ -187,7 +187,7 @@
                                     <p>Alain Nig</p>
                                 </td>
                                 <td>01-10-2021</td>
-                                <td><span class="status pending">Pending</span></td>
+                               
                             </tr>
                             <tr>
                                 <td>
@@ -195,7 +195,7 @@
                                     <p>Alain Nig</p>
                                 </td>
                                 <td>01-10-2021</td>
-                                <td><span class="status process">Process</span></td>
+                                
                             </tr>
                             <tr>
                                 <td>
@@ -203,7 +203,7 @@
                                     <p>Alain Nig</p>
                                 </td>
                                 <td>01-10-2021</td>
-                                <td><span class="status pending">Pending</span></td>
+                               
                             </tr>
                             <tr>
                                 <td>
@@ -211,14 +211,16 @@
                                     <p>Alain Nig</p>
                                 </td>
                                 <td>01-10-2021</td>
-                                <td><span class="status completed">Completed</span></td>
+
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <div class="todo">
                     <div class="head">
-                        <h3>Todos</h3>
+                        <h3>Recently Advice
+                            Posted
+                        </h3>
                         <i class='bx bx-plus'></i>
                         <i class='bx bx-filter'></i>
                     </div>
